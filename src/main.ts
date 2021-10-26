aa09a13a1634169551a3ff41e6d5d34712e3a8a3
sazhnev <rsazhniev@gloriumtech.com>
2021-10-26 14:14:44 +0300

    fix array field genre

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,13 @@ export async function bootstrap(): Promise<NestExpressApplication> {
     new ExpressAdapter(),
     { cors: true },
   );
+
+  const configService = app.select(SharedModule).get(ApiConfigService);
+
+  if (configService.documentationEnabled) {
+    setupSwagger(app);
+  }
+
   app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
   app.use(helmet());
   app.use(
@@ -63,8 +70,6 @@ export async function bootstrap(): Promise<NestExpressApplication> {
     }),
   );
 
-  const configService = app.select(SharedModule).get(ApiConfigService);
-
   // only start nats if it is enabled
   if (configService.natsEnabled) {
     const natsConfig = configService.natsConfig;
@@ -77,10 +82,6 @@ export async function bootstrap(): Promise<NestExpressApplication> {
     });
 
     await app.startAllMicroservices();
-  }
-
-  if (configService.documentationEnabled) {
-    setupSwagger(app);
   }
 
   const port = configService.appConfig.port;
